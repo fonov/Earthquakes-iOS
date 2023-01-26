0ab568bb7aed57a57c517880554ee20c178b7a0e
Sergei Fonov <sergeufonov@gmail.com>
2023-01-26 11:21:49 +0400

    Initial Commit

---
+++ Earthquake/EarthquakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakeApp.swift
+//  Earthquake
+//
+//  Created by Sergei Fonov on 26.01.23.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
